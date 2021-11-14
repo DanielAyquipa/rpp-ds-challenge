fd0ccae36b66b0f51fd71ed7460870367435042d
Daniel Alejandro Ayquipa Gonzales <54769441+DanielAyquipa@users.noreply.github.com>
2021-11-14 12:39:28 -0500

    Create ML_fraude.py

---
+++ src/ML_fraude.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 14 12:06:11 2021
+
+@author: danie
+"""
+
+import os
+import pandas as pd
+
+os.chdir(r"C:\Users\danie\Documents\GitHub\rpp-ds-challenge")
+
+df = pd.read_csv("ds_challenge_2021.csv")
+
+df.columns
+
+
